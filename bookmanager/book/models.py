42f53a41964e2c78c7f547c81ba3ce47baf31b63
qiansheng <980054594@qq.com>
2021-08-08 10:30:44 +0800

    添加第一个视图API

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -7,6 +7,10 @@ from django.db import models
 class BookInfo(models.Model):
     # 创建字段，字段类型...
     name = models.CharField(max_length=10)
+    
+    def __str__(self):
+        """将模型类以字符串的方式输出"""
+        return self.name
 
 
 # 准备人物列表信息的模型类
@@ -15,3 +19,7 @@ class PeopleInfo(models.Model):
     gender = models.BooleanField()
     # 外键约束：人物属于哪本书
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """将模型类以字符串的方式输出"""
+        return self.name
